d0780c0d88ef350e0b2e42186a53a438ba88c78a
andrebnu <79184116+andrebnu@users.noreply.github.com>
2021-08-31 10:40:52 -0300

    update form2
    Atualização 2 do forms

--- View/Form1.cs
+++ View/Form1.cs
@@ -16,5 +16,10 @@ namespace Trabalho_CacaPalavra
         {
             InitializeComponent();
         }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
