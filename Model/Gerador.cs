3161fd4c2db66ddaf279c3fdf991fc272259dcf1
Gustavo <gustavohs2004@gmail.com>
2021-08-31 10:40:21 -0300

    Gerador de botoes criado

--- Model/Gerador.cs
+++ Model/Gerador.cs
@@ -1,30 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using Trabalho_CacaPalavra.Model.Enums;
 
 namespace Trabalho_CacaPalavra.Model
 {
     class Gerador
     {
-        public static string[][] GeraBotoes()
+        public static void Botoes()
         {
-            string[][] botoes = new string[3][];
-            for (int i = 0; i < botoes.Length; i++)
-            {
-                botoes[i] = new string[3];
-            }
             Random ran = new Random();
-            for (int i = 0; i < botoes.Length; i++)
+            Form1 form = new Form1();
+            foreach (Control control in form.Controls)
             {
-                for (int j = 0; j < botoes[j].Length; j++)
+                Button button = control as Button;
+                int aleatorio = 0;
+                if (button == null) continue;
+                switch (button.Name)
                 {
-
+                    case "btnBotao00":
+                        aleatorio = ran.Next(0, 2);
+                        button.Text = ((Botao00)aleatorio).ToString();
+                        break;
+                    case "btnBotao01":
+                        aleatorio = ran.Next(0, 2);
+                        button.Text = ((Botao01)aleatorio).ToString();
+                        break;
+                    case "btnBotao02":
+                        aleatorio = ran.Next(0, 2);
+                        button.Text = ((Botao02)aleatorio).ToString();
+                        break;
+                    case "btnBotao10":
+                        aleatorio = ran.Next(0, 3);
+                        button.Text = ((Botao10)aleatorio).ToString();
+                        break;
+                    case "btnBotao11":
+                        aleatorio = ran.Next(0, 3);
+                        button.Text = ((Botao11)aleatorio).ToString();
+                        break;
+                    case "btnBotao12":
+                        aleatorio = ran.Next(0, 2);
+                        button.Text = ((Botao12)aleatorio).ToString();
+                        break;
+                    case "btnBotao20":
+                        aleatorio = ran.Next(0, 3);
+                        button.Text = ((Botao20)aleatorio).ToString();
+                        break;
+                    case "btnBotao21":
+                        aleatorio = ran.Next(0, 3);
+                        button.Text = ((Botao21)aleatorio).ToString();
+                        break;
+                    case "btnBotao22":
+                        aleatorio = ran.Next(0, 3);
+                        button.Text = ((Botao22)aleatorio).ToString();
+                        break;
                 }
             }
-
-            return botoes;
         }
     }
 }
